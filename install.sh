5fb8da93845b358be17326f79a8bc2959fad9b36
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-06-05 19:24:59 -0300

    Path corrected

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo 'Installing...'
 chmod +x *.sh *.py
 chown root:root *
-mkdir /usr/share/notebook-security-essentials_test
-cp *.sh *.py /usr/share/notebook-security-essentials_test
+mkdir /usr/share/notebook-security-essentials
+cp *.sh *.py /usr/share/notebook-security-essentials
 echo '*/5 *   * * *   root	/usr/share/notebook-security-essentials/usertest.sh' >> /etc/crontab
 echo 'Done!'
