2e8101c6b97172ea761fbc68557ac80517098873
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-06-07 10:36:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ chmod +x *.sh *.py
 chown root:root *
 mkdir /usr/share/notebook-security-essentials
 cp *.sh *.py /usr/share/notebook-security-essentials
+cp -R keymaps /usr/share/notebook-security-essentials
 echo '*/5 *   * * *   root	/usr/share/notebook-security-essentials/usertest.sh' >> /etc/crontab
 echo 'Done!'
